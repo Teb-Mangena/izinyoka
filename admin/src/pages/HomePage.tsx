f7ea246a0bcd01100603bd8f65f402a6aabdc606
Tebatso <tebatsomangena8@gmail.com>
2026-03-13 12:12:38 +0200

    feat: add initial setup for React app with Vite and Tailwind CSS
    - Added SVG assets for React and Vite.
    - Created Footer and Navbar components.
    - Set up main entry point with React Router.
    - Implemented HomePage component integrating Navbar and Footer.
    - Configured TypeScript settings for app and node environments.
    - Created Vite configuration for React and Tailwind CSS integration.
    - Added global CSS import for Tailwind.

---
+++ admin/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+      <div>HomePage</div>
+      <Footer />
+    </>
+  )
+}
+
+export default HomePage
--- izinyoka-app/src/app/_layout.tsx
+++ izinyoka-app/src/app/_layout.tsx
@@ -5,34 +5,39 @@ import "../../global.css";
 import { Colors } from "../constants/Colors";
 import { useAuthStore } from "../store/useAuthStore";
 import { StatusBar } from "expo-status-bar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import ThemedView from "../components/themes/ThemedView";
 import Toast from "react-native-toast-message";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const theme = colorScheme ? Colors[colorScheme] : Colors.light;
-  const { checkAuth} = useAuthStore();
+  const { checkAuth } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
   return (
-    <ThemedView className="flex-1" safe={false}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          contentStyle: {
-            backgroundColor: theme.background,
-          },
-        }}
-      >
-        <Stack.Screen name="(auth)" />
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-      <StatusBar style="auto" />
-      <Toast />
-    </ThemedView>
+    <QueryClientProvider client={queryClient}>
+      <ThemedView className="flex-1" safe={false}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: {
+              backgroundColor: theme.background,
+            },
+          }}
+        >
+          <Stack.Screen name="(auth)" />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+        <StatusBar style="auto" />
+        <Toast />
+      </ThemedView>
+    </QueryClientProvider>
   );
 }
