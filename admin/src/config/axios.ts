07749afa8a5cb2c003b211c97d89928b144d3f4c
Tebatso <tebatsomangena8@gmail.com>
2026-03-17 15:43:05 +0200

    delete clientENV

--- admin/src/config/axios.ts
+++ admin/src/config/axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
-import { clientENV } from "./clientEnv";
 
 export const axiosInstance = axios.create({
-  baseURL: clientENV.appUrl,
+  baseURL: import.meta.env.VITE_APP_URL,
   withCredentials: true
 })
--- admin/src/config/clientENV.ts
+++
@@ -1,3 +0,0 @@
-export const clientENV = {
-  appUrl: import.meta.env.VITE_APP_URL
-}
